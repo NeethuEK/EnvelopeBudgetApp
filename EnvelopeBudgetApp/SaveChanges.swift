c9d51fe945ae18ffde788e1cbcbdfab414882a04
uhteen21 <neethuk109@gmail.com>
2023-02-01 15:39:56 -0500

    Start changes for paymentPeriod

---
+++ EnvelopeBudgetApp/SaveChanges.swift
@@ -0,0 +1,8 @@
+//
+//  SaveChanges.swift
+//  EnvelopeBudgetApp
+//
+//  Created by Neethu Kuruvilla on 1/27/23.
+//
+
+import Foundation
---
+++ EnvelopeBudgetApp/Views/EnvelopeCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  EnvelopeCreateView.swift
+//  EnvelopeBudgetApp
+//
+//  Created by Neethu Kuruvilla on 1/24/23.
+//
+
+import SwiftUI
+
+struct EnvelopeCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EnvelopeCreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvelopeCreateView()
+    }
+}
