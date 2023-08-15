c55372d20a4c1ddb09f16fdde4378f5701acd992
uhteen21 <neethuk109@gmail.com>
2023-08-15 14:15:07 -0400

    Started added transactions

---
+++ EnvelopeBudgetApp/Views/Transactions/TransactionsCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionsCreateView.swift
+//  EnvelopeBudgetApp
+//
+//  Created by Neethu Kuruvilla on 8/15/23.
+//
+
+import SwiftUI
+
+struct TransactionsCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionsCreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionsCreateView()
+    }
+}
