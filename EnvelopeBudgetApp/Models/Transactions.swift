ead2afb9a25ef0fba6aa62bc03f7d5effc2a4e37
uhteen21 <neethuk109@gmail.com>
2023-10-11 14:31:39 -0400

    Created new merged transaction and preparing transaction enum

--- EnvelopeBudgetApp/Models/Transactions.swift
+++ EnvelopeBudgetApp/Models/Transactions.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
-protocol transaction{
-    
-    var date: Date {get set}
-}
-
-enum transaction{
-    case expense, additionalIncome
-}
+/*
+ enum transaction{
+ case expense(amount: Double, dateOf: Date,payee: String, title: String, note: String)
+ case additionalIncome(amount: Double, dateOf: Date,payer: String, title: String, note: String, goToEnvel: Bool, Envelope: Envelope?)
+ case reallocate(amount: Double, dateOf: Date,from: Envelope?,to: Envelope, title: String, note: String)
+ 
+ }
+ 
+ var expense = transaction.expense(amount: <#T##Double#>, dateOf: <#T##Date#>, payee: <#T##String#>, title: <#T##String#>, note: <#T##String#>)
+ */
+ 
